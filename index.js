c27600de58b7233bfd90bb2dac202c43ecd4e33a
Agent Hacker <43415449+AgentHackerYT@users.noreply.github.com>
2021-11-28 11:28:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-U cant see the codes!!!!
+const responses = require('./response');
+
+
+    const Scripts = (limit = 1 , showCount = false)=>{
+        let arr = []
+        var i = 0
+        var j = 1
+        if(showCount == true){
+            while(limit !== i){
+                const return1 = Generate()
+            arr.push(`${j++}) ${return1}`)
+            i++
+            }
+        }else{
+        while(limit !== i){
+            const return1 = Generate()
+        arr.push(return1)
+        i++
+        }
+    }
+    return arr
+    }
+function Generate(){
+    const answersArray = responses.scripts;
+    const answer = answersArray[Math.floor(Math.random() * answersArray.length)];
+    return answer
+}
+module.exports = {
+    Scripts
+}
